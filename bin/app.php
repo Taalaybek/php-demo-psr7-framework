95cde1edd4f074117ba3113457268c66c2a180f4
ElisDN <mail@elisdn.ru>
2018-06-21 13:22:04 +0300

    Enabled Doctrine console commands

--- bin/app.php
+++ bin/app.php
@@ -13,6 +13,17 @@ $container = require 'config/container.php';
 
 $cli = new Application('Application console');
 
+$entityManager = $container->get(\Doctrine\ORM\EntityManagerInterface::class);
+$connection = $entityManager->getConnection();
+
+$cli->setHelperSet(new Symfony\Component\Console\Helper\HelperSet([
+    'db' => new Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($connection),
+    'em' => new Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($entityManager),
+    'dialog' => new Symfony\Component\Console\Helper\QuestionHelper(),
+]));
+
+Doctrine\ORM\Tools\Console\ConsoleRunner::addCommands($cli);
+
 $commands = $container->get('config')['console']['commands'];
 foreach ($commands as $command) {
     $cli->add($container->get($command));
